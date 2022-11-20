780227a20876ce8fa95c123cc631f0301c749564
olasrum <mursalo@gmail.com>
2022-11-20 13:13:06 +1000

    Done with assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown (number)
+    x = number
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+puts countdown(10)
+
+def countdown_with_sleep (number)
+    x = number
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+        sleep 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+puts countdown_with_sleep(10)
